f00bfa920ca0bb0ee21c246f75e429c4922262d0
이진우 <dlwlsdn8864@naver.com>
2024-08-26 17:31:33 +0900

    캘린더 제작 "date-fns"와 "react-datepicker"다운필요

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,30 @@
 import React from "react";
 import './HomePage.css';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // useNavigate
+
+import DatePicker, { registerLocale } from 'react-datepicker'; //캘린더 라이브러리
+import ko from 'date-fns/locale/ko';
+import 'react-datepicker/dist/react-datepicker.css';
+
 import '../index.css';
+
+
+registerLocale('ko', ko); //캘린더 한글화
 
 function HomePage(){
   let [지출, 지출변경] = useState(100000);
   let [수입, 수입변경] = useState(200000);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const navigate = useNavigate(); // useNavigate 훅 사용
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    const formattedDate = date.toISOString().split('T')[0]; // 날짜를 'YYYY-MM-DD' 형식으로 포맷팅
+    navigate(`/moneychange?date=${formattedDate}`); // MoneyChange 페이지로 이동
+  };
+
+
     return(
     <div className="page">
       <div className="income-expense">
@@ -14,6 +33,16 @@ function HomePage(){
         <span className="divider"></span> {/* 수직선 역할 */}
         <span className="income">수입 금액<br/>{수입}원</span>
         <span className="divider"></span> {/* 수직선 역할 */}
+      </div>
+
+      <div className="calendar-container">
+          <DatePicker
+            locale="ko"
+            selected={selectedDate}
+            onChange={handleDateClick}
+            inline
+            calendarClassName="custom-datepicker" //캘린더 모양
+          />
       </div>
     </div>
     
--- src/pages/MoneyChange.js
+++ src/pages/MoneyChange.js
@@ -3,14 +3,17 @@ import { useState } from 'react';
 import './MoneyChange.css';
 import MoneyChangeButton from "../components/MoneyChangeButton";
 import FloatingButton from "../components/FloatingButton";
+import { useLocation } from 'react-router-dom'; // useLocation 훅 사용
 import '../index.css';
 
 
 function MoneyChange(){
-    let [지출, 지출변경] = useState(100000);
-    let [수입, 수입변경] = useState(200000);
+    const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const date = queryParams.get('date'); // 내가 선택한 날짜의 정보
     return(
         <div className="page">
+            <h1>{date}</h1>
             <MoneyChangeButton date="2024-08-23" text="일단은 homepage로 이동하게 설정" amount="$100" type={1} />
             <MoneyChangeButton date="2024-08-24" text="집주인 월세" amount="$100" type={0}/>
             <FloatingButton />
